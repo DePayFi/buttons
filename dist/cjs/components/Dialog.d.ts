0aef0189f105d2e4e2e33236caf91bf89be2c828
Sebastian Pape <spape@users.noreply.github.com>
2021-03-11 16:57:33 +0100

    V1 (#1)
    * documentation
    
    * fix: lint
    
    * fix build
    
    * move file to clean cache
    
    * move file to clean file cache
    
    * build v1
    
    * v1.0.0
    
    * v1

--- dist/cjs/components/Dialog.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface DialogProps {
-    content: React.ReactNode;
-}
-declare class Dialog extends React.Component<DialogProps> {
-    render(): React.ReactNode;
-}
-export default Dialog;
--- dist/umd/utils/ShadowContainer.d.ts
+++
@@ -1,6 +0,0 @@
-interface ShadowContainerParameters {
-    document: Document;
-    styles?: string;
-}
-export default function ShadowContainer({ document, styles, }: ShadowContainerParameters): HTMLElement;
-export {};
