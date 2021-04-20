c8f7c8af301eeadeee4556d8aaf78306508cbcf3
Sebastian Pape <s@pape.io>
2021-04-20 13:47:21 +0200

    split package into bundled/unbundled

--- rollup.config.js
+++ rollup.config.js
@@ -22,8 +22,8 @@ export default {
     },
   ],
   external: [
-    // ...Object.keys(pkg.dependencies || {}),
-    // ...Object.keys(pkg.peerDependencies || {}),
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     resolve(),
