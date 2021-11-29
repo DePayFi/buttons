7ba76eb0def081c1c5d53fe82fda6dc2d57a13f2
Sebastian Pape <s@pape.io>
2021-11-29 09:51:32 +0100

    v4.4.0: upgrade widgets to 4.4.0

--- rollup.globals.js
+++ rollup.globals.js
@@ -2,20 +2,5 @@ export default {
   'react': 'React',
   'react-dom': 'ReactDOM',
   '@depay/widgets': 'DePayWidgets',
-  'ethers': 'ethers',
-  'depay-react-dialog-stack': 'ReactDialogStack',
-  'depay-react-dialog': 'ReactDialog',
-  'depay-react-shadow-dom': 'ReactShadowDOM',
-  'depay-web3-blockchains': 'Web3Blockchains',
-  'depay-web3-wallets': 'Web3Wallets',
-  'depay-web3-payments': 'Web3Payments',
-  'depay-web3-tokens': 'Web3Tokens',
-  'depay-web3-client': 'Web3Client',
-  'depay-web3-exchanges': 'Web3Exchanges',
-  'depay-web3-constants': 'Web3Constants',
-  'depay-local-currency': 'LocalCurrency',
-  'depay-react-token-image': 'ReactTokenImage',
-  'react-error-boundary': 'ErrorBoundary',
-  'react-rangeslider': 'ReactRangeslider',
-  '@depay/walletconnect': 'WalletConnect'
+  'ethers': 'ethers'
 }
