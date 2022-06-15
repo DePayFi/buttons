83d57b20dd7f20af5079bed776663e382198fc8a
Sebastian Pape <s@pape.io>
2022-06-15 09:52:47 +0200

    v7.1.2: update dependencies and fix ReactDOMClient createRoot warning

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import DePayButton from './components/DePayButton'
 import DePayWidgets from '@depay/widgets'
 import init from './init'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import * as ReactDOMClient from 'react-dom/client'
 
 let DePayButtons = {
   init,
   DePayButton,
   React,
-  createRoot,
+  createRoot: ReactDOMClient.createRoot,
   DePayWidgets
 }
 
